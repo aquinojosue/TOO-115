ee774bb8172c473c7b76728342fa7291d8acb427
Josue Aquino <aquinojoue3@gmail.com>
2019-10-17 10:09:39 -0600

    Agregado modificacion roles usuarios

--- grails-app/controllers/sondeoprecios/PruebaController.groovy
+++ grails-app/controllers/sondeoprecios/PruebaController.groovy
@@ -1,6 +1,31 @@
 package sondeoprecios
+import SondeoPrecios.*
 
 class PruebaController {
 
-    static scaffold = SondeoPrecios.Usuario
+    def index(){
+        def userRoles = UsuarioRol.list()
+        [userRoles:userRoles]
+    }
+
+    def edit(){
+        def usuario = Usuario.get(params.id)
+        [roles:Rol.list(), usuario:usuario, selected:UsuarioRol.findByUsuario(usuario).rol, userID:params.id]
+    }
+
+    def save(){
+        def usuario = Usuario.get(params.userID)
+        def rolAnterior = UsuarioRol.findByUsuario(usuario).rol
+        def rolNuevo = Rol.findByAuthority(params.chosenRole)
+
+        UsuarioRol.remove usuario, rolAnterior
+        UsuarioRol.create usuario, rolNuevo
+
+        UsuarioRol.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        redirect(action: "index")
+    }
 }
--- grails-app/init/sondeoprecios/BootStrap.groovy
+++ grails-app/init/sondeoprecios/BootStrap.groovy
@@ -7,10 +7,12 @@ class BootStrap {
 
     def init = { servletContext ->
     	def roleAdmin = new Rol(authority: 'ROLE_ADMIN').save()
-		def adminUser = new Usuario(username: 'admin', password: 'admin123').save()    	
+		def adminUser = new Usuario(username: 'admin', password: 'admin123').save()
+		def adminUser2 = new Usuario(username: 'admin2', password: 'admin123').save()        	
     
 		UsuarioRol.create adminUser, roleAdmin
-    
+        UsuarioRol.create adminUser2, roleAdmin
+        
 		UsuarioRol.withSession {
 			it.flush()
 			it.clear()
